51a5c9a13139ff24cc27c141034f18b712aadfe0
Paul <paulvolkov1995@gmail.com>
2020-04-29 21:53:36 +0300

    add example content

---
+++ CS/DxBlazorApp/Components/MyGrid.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Blazor;
+using DxBlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DxBlazorApp.Components {
+    public class MyGrid<T> : ComponentBase {
+        [Parameter]
+        public IEnumerable<T> Data { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public Dictionary<string, object> Settings { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder) {
+            builder.OpenComponent<DxDataGrid<T>>(0);
+            builder.AddAttribute(1, "Data", (object)Data);
+            builder.AddAttribute(2, "Columns", ChildContent);
+            if (Settings != null) {
+                builder.AddMultipleAttributes(3, Settings);
+                //OR
+                //int seq = 3;
+                //foreach (var item in Settings) {
+                //    builder.AddAttribute(seq++, item.Key, item.Value);
+                //}
+            }
+            builder.CloseComponent();
+        }
+    }
+}
---
+++ CS/DxBlazorApp/Shared/MyGrid.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace DxBlazorApp.Shared
+{
+    public class MyGridBase<T> : DxDataGrid<T>
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<String, object> InputAttributes { get; set; }
+    }
+}
